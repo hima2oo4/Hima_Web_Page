96f8d681e9fa708fd32a6293fc9473886c817633
hima2oo4 <himapolamarasetty2004@outlook.com>
2025-09-01 17:22:27 +0530

    updated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://hima2oo4.github.io',
+    base: '/Hima_Web_Page',
     vite: {
         plugins: [tailwindcss()],
     },
